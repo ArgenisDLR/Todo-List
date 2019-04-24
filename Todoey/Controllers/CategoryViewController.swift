d0557b4d9c03c335584309fb55d2acf52ab52316
Argenis De La Rosa <hennyrosexo@gmail.com>
2019-04-24 16:20:57 -0400

    updated the swipeable VC

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -8,35 +8,55 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
     
     let realm = try! Realm()
     
-    var categories: Results<Category>!
+    var categories: Results<Category>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         loadCategories()
+        
+        tableView.separatorStyle = .none
+        
     }
     
-    //MARK: - Table View SETUP
+    //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        
+        return categories?.count ?? 1
+        
     }
     
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
-        cell.textLabel?.text = categories[indexPath.row].name
+        if let category = categories?[indexPath.row] {
+            
+            cell.textLabel?.text = category.name
+            
+            guard let categoryColour = UIColor(hexString: category.colour) else {fatalError()}
+            
+            cell.backgroundColor = categoryColour
+            cell.textLabel?.textColor = ContrastColorOf(categoryColour, returnFlat: true)
+            
+        }
+        
+        
         
         return cell
+        
     }
     
-    //MARK: - Table View Delegate Methods
+    
+    //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "goToItems", sender: self)
@@ -46,32 +66,50 @@ class CategoryViewController: UITableViewController {
         let destinationVC = segue.destination as! TodoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
         }
     }
     
-    //MARK: - Table Data Manipulation
+    //MARK: - Data Manipulation Methods
     
     func save(category: Category) {
-        
         do {
             try realm.write {
                 realm.add(category)
             }
         } catch {
-            print("Error saving categories \(error)")
+            print("Error saving category \(error)")
         }
+        
         tableView.reloadData()
+        
     }
     
     func loadCategories() {
-
-        categories = realm.objects(Category.self)
+        
+        categories  = realm.objects(Category.self)
         
         tableView.reloadData()
+        
     }
     
-    //MARK: - ADD NEW CATEGORIES
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
+    }
+    
+    
+    //MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
@@ -83,8 +121,7 @@ class CategoryViewController: UITableViewController {
             
             let newCategory = Category()
             newCategory.name = textField.text!
-            
-            self.categories.append(newCategory)
+            newCategory.colour = UIColor.randomFlat.hexValue()
             
             self.save(category: newCategory)
             
@@ -92,11 +129,13 @@ class CategoryViewController: UITableViewController {
         
         alert.addAction(action)
         
-        alert.addTextField { (text) in
+        alert.addTextField { (field) in
             textField = field
-            textField.placeholder = "Add a New Category"
+            textField.placeholder = "Add a new category"
         }
         
         present(alert, animated: true, completion: nil)
+        
     }
+    
 }
